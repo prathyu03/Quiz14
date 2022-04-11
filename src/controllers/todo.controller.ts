7d2801352ed7955309bbd78a6ef93ee01e918fb3
prathyu03 <sai.prathyu3@gmail.com>
2022-04-10 22:47:34 -0400

    commit

---
+++ src/controllers/todo.controller.ts
@@ -0,0 +1,81 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  put,
+  del,
+  requestBody,
+  response,
+} from '@loopback/rest';
+import {Todo} from '../models';
+import {TodoRepository} from '../repositories';
+
+export class TodoController {
+  constructor(
+    @repository(TodoRepository)
+    public todoRepository : TodoRepository,
+  ) {}
+
+  @get('/todos/{id}')
+  @response(200, {
+    description: 'Todo model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(Todo, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.number('id') id: number,
+    @param.filter(Todo, {exclude: 'where'}) filter?: FilterExcludingWhere<Todo>
+  ): Promise<Todo> {
+    return this.todoRepository.findById(id, filter);
+  }
+
+  @patch('/todos/{id}')
+  @response(204, {
+    description: 'Todo PATCH success',
+  })
+  async updateById(
+    @param.path.number('id') id: number,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Todo, {partial: true}),
+        },
+      },
+    })
+    todo: Todo,
+  ): Promise<void> {
+    await this.todoRepository.updateById(id, todo);
+  }
+
+  @put('/todos/{id}')
+  @response(204, {
+    description: 'Todo PUT success',
+  })
+  async replaceById(
+    @param.path.number('id') id: number,
+    @requestBody() todo: Todo,
+  ): Promise<void> {
+    await this.todoRepository.replaceById(id, todo);
+  }
+
+  @del('/todos/{id}')
+  @response(204, {
+    description: 'Todo DELETE success',
+  })
+  async deleteById(@param.path.number('id') id: number): Promise<void> {
+    await this.todoRepository.deleteById(id);
+  }
+}
